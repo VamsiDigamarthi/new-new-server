f43e13c92a6fb6715db5c1bb0348b18d07835a88
ravi <software.tarinee1@brihaspathi.com>
2025-06-23 18:07:53 +0530

    auth apis added

--- src/Routes/AuthRoute.js
+++ src/Routes/AuthRoute.js
@@ -1,18 +1,23 @@
 import express from "express";
 import {
   empRegisterUser,
+  getProfile,
   getUsersController,
   loginController,
   signupController,
+  updateUser,
 } from "../Controller/AuthController.js";
 import { validate } from "../Utils/validaters.js";
 import { loginSchema, signupSchema } from "../Validations/AuthValidation.js";
+import { authenticateToken } from "../Middlewares/AuthMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", validate(signupSchema), signupController);
 router.post("/login", validate(loginSchema), loginController);
 router.get("/users", getUsersController);
+router.patch("/users/:id", updateUser);
 router.post("/emp-register", empRegisterUser);
+router.get("/profile", authenticateToken, getProfile);
 
 export default router;
